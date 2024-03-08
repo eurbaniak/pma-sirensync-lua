3748c9f5705583da614317bde0d850076a702a00
Edwin <50717284+Adderall-dev@users.noreply.github.com>
2024-03-08 11:27:00 +0100

    init

--- client/client.lua
+++ client/client.lua
@@ -1,25 +1,14 @@
-local function toggleNuiFrame(shouldShow)
-  SetNuiFocus(shouldShow, shouldShow)
-  SendReactMessage('setVisible', shouldShow)
+local isNuiVisible = false
+
+local function toggleNuiFrame()
+  isNuiVisible = not isNuiVisible
+  SendReactMessage('setVisible', isNuiVisible)
 end
 
-RegisterCommand('show-nui', function()
-  toggleNuiFrame(true)
-  debugPrint('Show NUI frame')
-end)
-
+RegisterCommand('+toggleNui', toggleNuiFrame)
 RegisterNUICallback('hideFrame', function(_, cb)
-  toggleNuiFrame(false)
-  debugPrint('Hide NUI frame')
+  toggleNuiFrame()
   cb({})
 end)
 
-RegisterNUICallback('getClientData', function(data, cb)
-  debugPrint('Data sent by React', json.encode(data))
-
--- Lets send back client coords to the React frame for use
-  local curCoords = GetEntityCoords(PlayerPedId())
-
-  local retData <const> = { x = curCoords.x, y = curCoords.y, z = curCoords.z }
-  cb(retData)
-end)
+RegisterKeyMapping('+toggleNui', 'Toggle NUI frame', 'keyboard', '/')
--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -3,29 +3,12 @@ import "./App.css";
 import { debugData } from "../utils/debugData";
 import { fetchNui } from "../utils/fetchNui";
 
-// This will set the NUI to visible if we are
-// developing in browser
 debugData([
   {
     action: "setVisible",
     data: true,
   },
 ]);
-
-interface ReturnClientDataCompProps {
-  data: unknown;
-}
-
-const ReturnClientDataComp: React.FC<ReturnClientDataCompProps> = ({
-  data,
-}) => (
-  <>
-    <h5>Returned Data:</h5>
-    <pre>
-      <code>{JSON.stringify(data, null)}</code>
-    </pre>
-  </>
-);
 
 interface ReturnData {
   x: number;
@@ -50,15 +33,8 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="nui-wrapper">
-      <div className="popup-thing">
-        <div>
-          <h1>This is the NUI Popup!</h1>
-          <p>Exit with the escape key</p>
-          <button onClick={handleGetClientData}>Get Client Data</button>
-          {clientData && <ReturnClientDataComp data={clientData} />}
-        </div>
-      </div>
+    <div className="control-wrapper">
+      <div className="control"></div>
     </div>
   );
 };
